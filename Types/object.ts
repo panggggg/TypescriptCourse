51834a16bbdf38c4f9d7bad7a72d40e8c2d84b04
panggggg <p.picczy@gmail.com>
2021-12-04 00:16:28 +0700

    Add object.ts

---
+++ Types/object.ts
@@ -0,0 +1,17 @@
+const personObj: {name: string, age: number} = {
+    name: "Pawornwan",
+    age: 21
+}
+
+console.log(personObj.name)
+console.log(personObj.age)
+
+
+
+// const person = {
+//     name: "Pawornwan",
+//     age: 21
+// }
+
+// console.log(person.name)
+// console.log(person.age)
