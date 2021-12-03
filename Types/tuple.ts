f0c67a93b9b233b832449d083741ee990ec426c1
panggggg <p.picczy@gmail.com>
2021-12-04 00:17:08 +0700

    Add tuple.ts

---
+++ Types/tuple.ts
@@ -0,0 +1,9 @@
+const personTuple = {
+    name: "Pawornwan",
+    age: 21,
+    hobbies: ['play game', 'cooking'],
+    role: [2, 'author']
+}
+
+personTuple.role.push('admin')
+personTuple.role[1] = 10
