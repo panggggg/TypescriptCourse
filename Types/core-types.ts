1cb873abf65ad13999aa8b831c9edd021206beee
panggggg <p.picczy@gmail.com>
2021-12-04 00:16:06 +0700

    Add object and array

--- Types/core-types.ts
+++ Types/core-types.ts
@@ -11,3 +11,13 @@ const isCat: boolean = true
 const isDog: boolean = false
 
 console.log(typeof(isCat)) // -> boolean
+
+//4. object (type of object)
+const obj: object = {age: 30}
+
+//5. array (type can be flexible or strict)
+const arr: number[] = [1, 2, 3]
+const colour: string[] = ['red', 'black']
+const any: any[] = ['hello', 10]
+
+//6. tuple (fixed length array)
