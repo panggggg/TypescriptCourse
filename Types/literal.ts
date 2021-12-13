a50303133e626b52b255e4e790d3cc9a32f23a75
panggggg <p.picczy@gmail.com>
2021-12-13 09:56:02 +0700

    Add literal.ts

---
+++ Types/literal.ts
@@ -0,0 +1,6 @@
+function printText(s: string, alignment: "left" | "right" | "center"){
+    
+}
+
+printText("Hello world", "left")
+// printText("G'day, mate", "centre") // type centre is not assignable
