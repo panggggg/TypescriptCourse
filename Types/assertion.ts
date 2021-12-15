95f42fd708aeeadec0eced34e1f9e9a9317e799e
panggggg <p.picczy@gmail.com>
2021-12-15 10:40:28 +0700

    Update assertion.ts

--- Types/assertion.ts
+++ Types/assertion.ts
@@ -2,11 +2,10 @@ let someValue: any = "some value"
 
 //assertion เป็นการระบุ type ตอนที่กำลังจะใช้งาน
 
+//as syntax
 let someValueLength: number = (someValue as string).length
-// or
-// let someValueLength: number = (<string>someValue).length
+// or angle-bracket syntax
+let strLength: number = (<string>someValue).length
 
 console.log(someValueLength) //10
-
-// const trustMe: number = '11'
-const trustMe: number = ('11' as unknown as number)
+console.log(strLength)
