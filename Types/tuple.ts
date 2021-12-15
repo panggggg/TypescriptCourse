b0bfd232ab6c83f623b7667050b7695ec801ea63
panggggg <p.picczy@gmail.com>
2021-12-15 10:41:52 +0700

    Update tuple.ts

--- Types/tuple.ts
+++ Types/tuple.ts
@@ -12,6 +12,8 @@ const personTuple: {
 
 personTuple.role.push('admin')
 personTuple.role[0] = 20
+console.log(personTuple.name.substring(6)) //output -> wan
+console.log(personTuple.role[1].substring(0, 2)) //output -> au
 console.log(personTuple)
 
 // personTuple.role[1] = 10 --> index 1 is string
